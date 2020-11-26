7f52a3f16dff109ef52b290fbd7d0050ead5c1e1
jogamer275 <jordygo4it@gmail.com>
2020-11-26 20:25:08 +0100

    Added HomeTheaterFacade and made some changes in Main.java

---
+++ src/app/HomeTheaterFacade.java
@@ -0,0 +1,90 @@
+package app;
+
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    DvdPlayer dvd;
+    CdPlayer cd;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(Amplifier amp,
+                             Tuner tuner,
+                             DvdPlayer dvd,
+                             CdPlayer cd,
+                             Projector projector,
+                             TheaterLights lights,
+                             Screen screen,
+                             PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.dvd = dvd;
+        this.cd = cd;
+        this.projector = projector;
+        this.screen = screen;
+        this.lights = lights;
+        this.popper = popper;
+    }
+
+    public void watchMovie(String movie) {
+        System.out.println("Get ready for the movie");
+        popper.on();
+        popper.pop();
+        lights.dim(10);
+        screen.down();
+        projector.on();
+        projector.wideScreenMode();
+        amp.on();
+        amp.setDvd(dvd);
+        amp.setSurroundSound();
+        amp.setVolume(5);
+        dvd.on();
+        dvd.play(movie);
+
+    }
+
+    public void endMovie() {
+        System.out.println("\nShutting down movie theater");
+        popper.off();
+        lights.on();
+        projector.off();
+        amp.off();
+        dvd.stop();
+        dvd.eject();
+        dvd.off();
+    }
+
+    public void listenToMusic(String title) {
+        System.out.println("\nGet ready for the music");
+        lights.on();
+        amp.on();
+        amp.setVolume(5);
+        amp.setCd(cd);
+        amp.setStereoSound();
+        cd.on();
+        cd.play(title);
+    }
+    public void endMusic(){
+        System.out.println("\nStop the music");
+        amp.off();
+        cd.eject();
+        cd.off();
+    }
+
+    public void listenToTheRadio(double frequency){
+        System.out.println("\nStart the radio");
+        tuner.on();
+        tuner.setFrequency(frequency);
+        amp.on();
+        amp.setVolume(5);
+        amp.setTuner(tuner);
+    }
+
+    public void endRadio(){
+        System.out.println("\nStop the radio");
+        tuner.off();
+        amp.off();
+    }
+}
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,13 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        int input;
+
         Amplifier amp = new Amplifier("Top-O-Line Amplifier");
         Tuner tuner = new Tuner("Top-O-Line AM/FM Tuner", amp);
         DvdPlayer dvd = new DvdPlayer("Top-O-Line DVD Player", amp);
@@ -11,5 +16,26 @@ public class Main {
         TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
         Screen screen = new Screen("Theater Screen");
         PopcornPopper popper = new PopcornPopper("Popcorn Popper");
+
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, tuner, dvd, cd, projector, lights, screen, popper);
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("In the homeTheater you have three options: ");
+        System.out.println("1. Press 1 to play a Movie");
+        System.out.println("2. Press 2 to Listen to music");
+        System.out.println("3. Press 3 to Listen to the radio");
+        input = console.nextInt();
+
+        if (input == 1) {
+            homeTheater.watchMovie("Raiders of the Lost Ark");
+            homeTheater.endMovie();
+        } else if (input == 2) {
+            homeTheater.listenToMusic("Andries' greatest dance tracks");
+            homeTheater.endMusic();
+        } else if (input == 3) {
+            homeTheater.listenToTheRadio(101);
+            homeTheater.endRadio();
+        }
     }
 }
